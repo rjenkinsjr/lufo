2b58bcf464741253c5239efa8f3c9496605a06d0
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2017-12-09 22:02:08 -0500

    Initial code commit

---
+++ UFO.js
@@ -0,0 +1,182 @@
+const UFOH = require('./UFOHelper.js');
+
+/*
+ * Constructor
+ */
+var UFO = module.exports = function(options, callback) {
+  this._options = options;
+
+  // UFOs will close connections after a cetain time of not receiving any data.
+  // TCP keepalives from Node don't seem to help.
+  // This flag is used by the "close" event handler to re-establish a connection
+  // that was unknowingly closed by the UFO.
+  //
+  // If this is true, this UFO instance is unusable and will no longer perform
+  // any UFO interaction methods (e.g. rgbw).
+  this._dead = false;
+
+  // Storage/tracking for the status response.
+  this._statusArray = new Uint8Array(UFOH.statusResponseSize);
+  this._statusIndex = 0;
+
+  // Setup the TCP socket.
+  UFOH.initializeClient(this);
+
+  // Connect now, if a callback was requested.
+  typeof callback === 'function' && this.connect(callback);
+};
+
+/*
+ * Core methods
+ */
+UFO.prototype.connect = function(callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  this._client.connect({
+    host: this._options.host,
+    port: UFOH.tcpPort
+  }, function() {
+    typeof callback === 'function' && callback();
+  });
+}
+UFO.prototype.disconnect = function(callback) {
+  // If already dead, just run the callback with no error.
+  if (this._dead) {
+    typeof callback === 'function' && callback();
+    return;
+  }
+  // We're intentionally closing this connection.
+  // Don't allow it to be used again.
+  this._dead = true;
+  this._client.unref();
+  this._disconnectCallback = callback;
+  this._client.end();
+}
+UFO.prototype.status = function(callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  this._client.resume();
+  this._statusCallback = function(err, data) {
+    typeof callback === 'function' && callback(err, data);
+    this._statusCallback = null;
+    this._client.pause();
+  }.bind(this);
+  this._client.write(UFOH.statusRequest);
+}
+UFO.prototype.on = function(callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  this._client.write(UFOH.onRequest, callback);
+}
+UFO.prototype.off = function(callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  this._client.write(UFOH.offRequest, callback);
+}
+
+/*
+ * Standard control methods
+ */
+UFO.prototype.rgbw = function(red, green, blue, white, callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  // 0x31 rr gg bb ww 0x00
+  // 0x00 seems to be a constant terminator for the data.
+  var buf = Buffer.alloc(6);
+  buf.writeUInt8(0x31, 0);
+  buf.writeUInt8(UFOH.clampRGBW(red), 1);
+  buf.writeUInt8(UFOH.clampRGBW(green), 2);
+  buf.writeUInt8(UFOH.clampRGBW(blue), 3);
+  buf.writeUInt8(UFOH.clampRGBW(white), 4);
+  buf.writeUInt8(0, 5);
+  this._client.write(UFOH.prepareBytes(buf), callback);
+}
+UFO.prototype.builtin = function(name, speed, callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  // 0x61 id speed
+  var buf = Buffer.alloc(3);
+  buf.writeUInt8(0x61, 0);
+  buf.writeUInt8(UFOH.functionIds[name], 1);
+  // This function accepts a speed from 0 (slow) to 100 (fast).
+  buf.writeUInt8(UFOH.flipSpeedBuiltin(speed), 2);
+  this._client.write(UFOH.prepareBytes(buf), callback);
+}
+UFO.prototype.custom = function(speed, mode, steps, callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  // Validate the mode.
+  var modeId;
+  switch (mode) {
+    case 'gradual':
+      modeId = 0x3a;
+      break;
+    case 'jumping':
+      modeId = 0x3b;
+      break;
+    case 'strobe':
+      modeId = 0x3c;
+      break;
+    default:
+      typeof callback === 'function' && callback(new Error(`Invalid mode '${mode}'.`));
+      break;
+  }
+  // 0x51 steps(16xUInt8) speed mode 0xff
+  // 0xff seems to be a constant terminator for the data.
+  var buf = Buffer.alloc(68);
+  buf.writeUInt8(0x51, 0);
+  var index = 1;
+  // Each step consists of an RGB value and is translated into 4 bytes.
+  // The 4th byte is always zero.
+  //
+  // If there are fewer than 16 steps, "null" steps must be added so we have
+  // exactly 16 steps. A "null" step is 0x01 0x02 0x03 0x00.
+  //
+  // Any null steps intermingled with non-null steps must be removed, as they
+  // will cause the pattern to stop. Null steps can only exist at the end of
+  // the array.
+  //
+  // While we're doing this, truncate the array to size 16.
+  var stepsCopy = steps.filter(function(s) {
+    return !(s.red === UFOH.nullStep.red &&
+             s.green === UFOH.nullStep.green &&
+             s.blue === UFOH.nullStep.blue);
+  }).slice(0, UFOH.customStepsSize);
+  while (stepsCopy.length < UFOH.customStepsSize) {
+    stepsCopy.push(UFOH.nullStep);
+  }
+  for (const step of stepsCopy) {
+    buf.writeUInt8(UFOH.clampRGBW(step.red), index);
+    index++;
+    buf.writeUInt8(UFOH.clampRGBW(step.green), index);
+    index++;
+    buf.writeUInt8(UFOH.clampRGBW(step.blue), index);
+    index++;
+    buf.writeUInt8(0, index);
+    index++;
+  }
+  // This function accepts a speed from 0 (slow) to 30 (fast).
+  buf.writeUInt8(UFOH.flipSpeedCustom(speed) + 1, index);
+  index++;
+  // Set the mode.
+  buf.writeUInt8(modeId, index);
+  index++;
+  // Add terminator and write.
+  buf.writeUInt8(0xff, index);
+  this._client.write(UFOH.prepareBytes(buf), callback);
+}
+
+/*
+ * Helper methods
+ */
+ UFO.prototype.getHost = function() {
+   return this._options.host;
+ }
+UFO.prototype.freeze = function(callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  this.builtin('noFunction', 0, callback);
+}
+UFO.prototype.zero = function(callback) {
+  if (UFOH.stopIfDead(this, callback)) return;
+  this.rgbw(0, 0, 0, 0, callback);
+}
+
+/*
+Disco and camera modes:
+  0x41 ?? ?? ?? ?? ?? 0x0f checksum
+irrelevant mode because it's dependent on the device's microphone or camera; individual transmissions just set the color
+only relevant observation is that 41 is the header
+*/
---
+++ UFOHelper.js
@@ -0,0 +1,331 @@
+const net = require('net');
+
+// All UFOs listen on the same ports.
+const tcpPort = 5577;
+const udpPort = 48899;
+
+// TCP socket (client) creation method.
+const initializeClient = function(ufo) {
+  // The TCP socket used to communicate with the UFO.
+  ufo._client = net.Socket();
+  ufo._error = null;
+  // Send all data immediately; no buffering.
+  ufo._client.setNoDelay(true);
+  // Capture the error so we can respond appropriately.
+  ufo._client.on('error', function(err) {
+    this._error = err;
+  }.bind(ufo));
+  // Both sides have FIN'ed. No more communication is allowed on this socket.
+  ufo._client.on('close', closeClient.bind(ufo));
+  // Any data received by the UFO is a status update.
+  ufo._client.on('data', statusReceive.bind(ufo));
+  // Initially, ignore all received data.
+  ufo._client.pause();
+}
+
+// If the UFO FIN'ed first due to inactivity, reconnect.
+// If the UFO FIN'ed first due to an error, reconnect if requested by the client.
+// Otherwise, fire the disconnect callback.
+//
+// Must be bound to a UFO instance.
+const closeClient = function() {
+  // If still alive, the UFO closed on its own.
+  // Otherwise, the client closed intentionally.
+  var reconnect = !this._dead;
+  if (reconnect) {
+    // Assume it was due to inactivity.
+    if (this._error) {
+      // UFO closed due to some error.
+      reconnect = this._options.reconnectOnError || false;
+      // If reconnect is true, print the error.
+      if (reconnect) {
+        console.log(`Error occurred on UFO ${this._options.host}, will reconnect as requested.`);
+        console.log(err, err.stack);
+      }
+    }
+  }
+  // Tear down the old socket.
+  this._client.destroy();
+  this._client.unref();
+  // Reconnect if necessary.
+  if (reconnect) {
+    initializeClient(this);
+    this.connect();
+  } else {
+    var callback = this._disconnectCallback;
+    typeof callback === 'function' && callback(this._error);
+  }
+}
+
+// Dead enforcement method.
+const stopIfDead = function(ufo, callback) {
+  if (ufo._dead) {
+    typeof callback === 'function' && callback(new Error(`UFO has been disconnected.`));
+  }
+  return ufo._dead;
+}
+
+// Given a buffer of data destined for a UFO, expands the buffer by 2 and
+// inserts the last two bytes (the "local" constant 0x0f, and the checksum).
+const prepareBytes = function(buf) {
+  var newBuf = Buffer.alloc(buf.length + 2);
+  buf.copy(newBuf);
+  finalizeBytes(newBuf);
+  checksumBytes(newBuf);
+  return newBuf;
+}
+// Adds the "local" flag to the given buffer.
+//
+// For virtually all datagrams sent to UFOs, the second-to-last byte is either
+// 0f (local) or (f0) remote. I'm pretty sure "remote" refers to UFOs that are
+// exposed to the Internet via the company's cloud service, which is not in the
+// scope of this library, so we always use "local".
+//
+// Expects a buffer that is sized for transmission to the UFO.
+const finalizeBytes = function(buf) {
+  buf.writeUInt8(0x0f, buf.length - 2);
+}
+// Adds the checksum to the buffer.
+//
+// Sum up all the values in the buffer, then divide by 256.
+// The checksum is the remainder.
+//
+// Expects a buffer that is sized for transmission to the UFO.
+const checksumBytes = function(buf) {
+  // Sanity.
+  var lastIndex = buf.length - 1;
+  buf.writeUInt8(0, lastIndex);
+  // Do it.
+  var checksum = 0;
+  for (const value of buf.values()) {
+    checksum += value;
+  }
+  buf.writeUInt8(checksum % 0x100, lastIndex);
+}
+
+// Status byte signature (no local flag or checksum)
+const statusRequest = Buffer.alloc(4);
+statusRequest.writeUInt8(0x81, 0);
+statusRequest.writeUInt8(0x8a, 1);
+statusRequest.writeUInt8(0x8b, 2);
+statusRequest.writeUInt8(0x96, 3);
+// Size of all status response messages.
+const statusResponseSize = 14;
+// Handler for receiving status bytes. Must be bound to a UFO instance.
+const statusReceive = function(data) {
+  // Add the data to what we already have.
+  var oldIndex = this._statusIndex;
+  var newIndex = oldIndex + data.length;
+  this._statusArray.set(data, oldIndex);
+  if (newIndex >= statusResponseSize) {
+    // We have the full response. Capture it and reset the storage buffer.
+    var responseBytes = Buffer.from(this._statusArray);
+    this._statusArray.fill(0);
+    // Reset the status response index.
+    newIndex = 0;
+    // Prepare callback variables.
+    var err = null;
+    var data = {};
+    // Verify the response's integrity.
+    if (responseBytes.readUInt8(0) === 0x81) {
+      // Compute the actual checksum.
+      var lastIndex = statusResponseSize - 1;
+      var expectedChecksum = responseBytes.readUInt8(lastIndex);
+      responseBytes.writeUInt8(0, lastIndex);
+      var actualChecksum = 0;
+      for (const value of responseBytes.values()) {
+        actualChecksum += value;
+      }
+      actualChecksum = actualChecksum % 0x100;
+      // Compare.
+      responseBytes.writeUInt8(expectedChecksum, lastIndex);
+      if (expectedChecksum !== actualChecksum) {
+        err = new Error('Status check failed (checksum mismatch).');
+      }
+    } else {
+      err = new Error('Status check failed (header mismatch).');
+    }
+
+    /*
+    Response format:
+    0x81 ???a POWER MODE ???b SPEED RED GREEN BLUE WHITE [UNUSED] CHECKSUM
+
+    ???a is unknown. It always seems to be 0x04.
+    ???b is unknown; it always seems to be 0x21.
+    [UNUSED] is a 3-byte big-endian field whose purpose is unknown. It always seems to be "0x03 0x00 0x00".
+    */
+
+    // Add raw bytes to the response.
+    data.raw = responseBytes;
+    // ON_OFF is always either 0x23 or 0x24.
+    if (!err) {
+      switch (responseBytes.readUInt8(2)) {
+        case 0x23:
+          data.power = 'on'
+          break;
+        case 0x24:
+          data.power = 'off';
+          break;
+        default:
+          err = new Error(`Status check failed (impossible power value ${power}).`);
+      }
+    }
+    // MODE:
+    // - 0x62 is disco mode or camera mode (called "other").
+    // - 0x61 is static color.
+    // - 0x60 is custom steps.
+    // - Otherwise, it is a function ID.
+    if (!err) {
+      var mode = responseBytes.readUInt8(3);
+      switch (mode) {
+        case 0x62:
+          data.mode = 'other';
+          break;
+        case 0x61:
+          data.mode = 'static';
+          break;
+        case 0x60:
+          data.mode = 'custom';
+          break;
+        default:
+          var found = false;
+          for (var f in functionIds) {
+            if (functionIds[f] === mode) {
+              data.mode = `function:${f}`;
+              found = true;
+              break;
+            }
+          }
+          if (!found) err = new Error(`Status check failed (impossible mode ${mode}).`);
+          break;
+      }
+    }
+    // SPEED is evaluated based on MODE, and it does not apply to all modes.
+    if (!err) {
+      var speed = responseBytes.readUInt8(5);
+      if (data.mode === 'custom') {
+        // The UFO seems to store/report the speed as 1 higher than what it really is.
+        data.speed = flipSpeedCustom(speed - 1);
+      }
+      if (data.mode.startsWith('function')) {
+        data.speed = flipSpeedBuiltin(speed);
+      }
+    }
+    // Capture RGBW values as-is.
+    if (!err) {
+      data.red = responseBytes.readUInt8(6);
+      data.green = responseBytes.readUInt8(7);
+      data.blue = responseBytes.readUInt8(8);
+      data.white = responseBytes.readUInt8(9);
+    }
+    // Transfer control to the user's callback.
+    if (err) data = null;
+    this._statusCallback(err, data);
+  }
+  // Update the status response index.
+  this._statusIndex = newIndex;
+}
+
+/*
+ * On/off byte signatures
+ */
+// 71 23 0F A3, always (this includes local flag and checksum)
+const onRequest = Buffer.alloc(4);
+onRequest.writeUInt8(0x71, 0);
+onRequest.writeUInt8(0x23, 1);
+onRequest.writeUInt8(0x0f, 2);
+onRequest.writeUInt8(0xa3, 3);
+// 71 24 0F A4, always (this includes local flag and checksum)
+const offRequest = Buffer.alloc(4);
+offRequest.writeUInt8(0x71, 0);
+offRequest.writeUInt8(0x24, 1);
+offRequest.writeUInt8(0x0f, 2);
+offRequest.writeUInt8(0xa4, 3);
+
+// Clamps RGBW values within the accepted range (0 <= value <= 255).
+const clampRGBW = function(num) {
+  return clamp(num, 0, 255);
+}
+
+// Built-in function names and IDs.
+const functionIds = Object.freeze({
+  sevenColorCrossFade: 0x25,
+  redGradualChange: 0x26,
+  greenGradualChange: 0x27,
+  blueGradualChange: 0x28,
+  yellowGradualChange: 0x29,
+  cyanGradualChange: 0x2a,
+  purpleGradualChange: 0x2b,
+  whiteGradualChange: 0x2c,
+  redGreenCrossFade: 0x2d,
+  redBlueCrossFade: 0x2e,
+  greenBlueCrossFade: 0x2f,
+  sevenColorStrobeFlash: 0x30,
+  redStrobeFlash: 0x31,
+  greenStrobeFlash: 0x32,
+  blueStrobeFlash: 0x33,
+  yellowStrobeFlash: 0x34,
+  cyanStrobeFlash: 0x35,
+  purpleStrobeFlash: 0x36,
+  whiteStrobeFlash: 0x37,
+  sevenColorJumpingChange: 0x38,
+  noFunction: 0x61
+});
+// Given a function name, returns its hex value.
+const getFunctionId = function(name) {
+  if (!functionIds.hasOwnProperty(name)) throw new Error(`No such built-in function '${name}'.`);
+  return functionIds[name];
+}
+// Converts a built-in function speed value back/forth between what the
+// user inputs and what is transmitted in the byte array.
+const flipSpeedBuiltin = function(speed) {
+  return Math.abs(clamp(speed, 0, 100) - 100);
+}
+
+// Maximum number of custom steps per single command.
+const customStepsSize = 16;
+// Null step, used in custom commands.
+const nullStep = Object.freeze({
+  red: 1,
+  green: 2,
+  blue: 3
+});
+// Converts a custom command speed value back/forth between what the
+// user inputs and what is transmitted in the byte array.
+const flipSpeedCustom = function(speed) {
+  return Math.abs(clamp(speed, 0, 30) - 30);
+}
+
+/*
+ * Miscellaneous functions
+ */
+const clamp = function(num, min, max) {
+  if (min > max) throw new Error(`Min ${min} greater than max ${max}`);
+  return num <= min ? min : num >= max ? max : num;
+}
+
+/*
+ * Public export
+ */
+var UFOHelper = module.exports = Object.freeze({
+  tcpPort: tcpPort,
+  udpPort: udpPort,
+  initializeClient: initializeClient,
+  stopIfDead: stopIfDead,
+  prepareBytes: prepareBytes,
+  finalizeBytes: finalizeBytes,
+  checksumBytes: checksumBytes,
+  statusRequest: statusRequest,
+  statusResponseSize: statusResponseSize,
+  statusReceive: statusReceive,
+  onRequest: onRequest,
+  offRequest: offRequest,
+  clampRGBW: clampRGBW,
+  functionIds: functionIds,
+  getFunctionId: getFunctionId,
+  flipSpeedBuiltin: flipSpeedBuiltin,
+  customStepsSize: customStepsSize,
+  nullStep: nullStep,
+  flipSpeedCustom: flipSpeedCustom,
+  clamp: clamp
+});
