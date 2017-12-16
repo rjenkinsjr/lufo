cebb2243c35868877225dcd100f4bbcac4e26e05
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2017-12-15 22:08:48 -0500

    Add/document AT+Q

--- udp/Constants.js
+++ udp/Constants.js
@@ -11,6 +11,13 @@ const receiveNoData = ack + receiveSuffix;
 // A map of commands to their send/receive syntax.
 const commandSet = Object.freeze({
   ok: { send: ack },
+  // Not documented, nor does it show up in AT+H output, but probably
+  // means "quiet mode off". It gets used a lot by the Android app,
+  // particularly when doing a factory reset or WiFi reconfiguration.
+  //
+  // It doesn't seem to be strictly necessary for other functions, so
+  // it is declared here only for completeness.
+  quiet: { send: 'AT+Q\r' },
   reboot: { send: 'AT+Z\r' },
   factoryReset: {
     send: 'AT+RELD\r',
