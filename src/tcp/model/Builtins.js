4c1cd93848d49759bba3d058a820baed3a80fd75
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-03-10 20:49:19 -0500

    Cleanup TCP builtins class definition

--- src/tcp/model/Builtins.js
+++ src/tcp/model/Builtins.js
@@ -2,7 +2,12 @@
 const _ = require('lodash');
 const { Map, Set } = require('immutable');
 const fs = require('fs');
+
+/* Private variables */
 const builtinMap = Map(JSON.parse(fs.readFileSync(__dirname + '/builtins.json', 'utf8'))).map(v => parseInt(v, 16));
+const specialFunctionNames = Set.of('noFunction', 'postReset');
+const functionNames = builtinMap.keySeq().toSet().subtract(specialFunctionNames);
+const maxSpeed = 100;
 
 /**
  * This class contains methods for utilizing a UFO's built-in functions. UFOs come with 20 such functions.
@@ -10,20 +15,13 @@ const builtinMap = Map(JSON.parse(fs.readFileSync(__dirname + '/builtins.json', 
 class Builtins {
   /**
    * Returns the map of built-in function names to IDs. Function IDs are hexadecimal numbers.
+   * This map includes internal function names that are excluded by {@link Builtins#getFunctionNames}.
    */
-  getFunctions(): Map<string, number> {
-    return builtinMap;
-  }
+  getFunctions(): Map<string, number> { return builtinMap; }
   /**
-   * These functions should be used only internally; they are omitted from {@link Builtins#getFunctionNames}.
+   * Returns the set of valid UFO built-in function names usable by the CLI/API.
    */
-  getSpecialFunctionNames(): Set<string> { return Set.of('noFunction', 'postReset'); }
-  /**
-   * Returns the set of valid UFO built-in function names. Does not include {@link Builtins#getSpecialFunctionNames}.
-   */
-  getFunctionNames(): Set<string> {
-    return this.getFunctions().keySeq().toSet().subtract(this.getSpecialFunctionNames());
-  }
+  getFunctionNames(): Set<string> { return functionNames; }
   /**
    * Given a built-in function name, returns its hexadecimal ID.
    * @throws {Error} if an invalid function name is provided.
@@ -37,7 +35,7 @@ class Builtins {
    * Input and output are clamped to 0-100 inclusive.
    */
   flipSpeed(speed: number): number {
-    return Math.abs(_.clamp(speed, 0, 100) - 100);
+    return Math.abs(_.clamp(speed, 0, maxSpeed) - maxSpeed);
   }
 }
 
