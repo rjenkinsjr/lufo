4ea4467fc0cce813643b22bdf8109bfea9ea7538
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-03-29 23:21:18 -0400

    Fix parallel

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,7 +3,7 @@ set +e
 . ~/lufo/.lufoDependencies
 PARALLEL_ARGFILE="/tmp/lufoTest-$RANDOM"
 for v in "${LUFO_NODE_VERSIONS[@]}"; do
-  echo "exec $v yarn run api-test" >> $PARALLEL_ARGFILE
-  echo "exec $v yarn run cli-test" >> $PARALLEL_ARGFILE
+  echo "nvm exec $v yarn run api-test" >> $PARALLEL_ARGFILE
+  echo "nvm exec $v yarn run cli-test" >> $PARALLEL_ARGFILE
 done
-parallel -k --ungroup nvm :::: $PARALLEL_ARGFILE
+parallel -k --ungroup :::: $PARALLEL_ARGFILE
