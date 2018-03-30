7475d2c2f981869c0ecf11d38d74e9a4d95b17d0
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-03-30 00:05:02 -0400

    Improve formatting

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set +e
+HEADER="false"
 . ~/lufo/.lufoDependencies
 for v in "${LUFO_NODE_VERSIONS[@]}"; do
+  if [[ "$HEADER" == "false" ]]; then
+    HEADER="true"
+  else
+    echo; echo; echo
+  fi
   echo "=== $v ==="
   echo '--- API ---'
   nvm exec $v yarn run api-test
   echo '--- CLI ---'
   nvm exec $v yarn run cli-test
-  echo
 done
