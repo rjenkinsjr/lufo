75db8ca8c59eed7bcd64149fdaff24e46c2ae97b
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-03-29 23:34:53 -0400

    Try to prevent log interleaving

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,7 +3,9 @@ set +e
 . ~/lufo/.lufoDependencies
 PARALLEL_ARGFILE="/tmp/lufoTest-$RANDOM"
 for v in "${LUFO_NODE_VERSIONS[@]}"; do
-  echo "nvm exec $v yarn run api-test" >> $PARALLEL_ARGFILE
-  echo "nvm exec $v yarn run cli-test" >> $PARALLEL_ARGFILE
+  echo "nvm exec $v yarn run api-test > ${PARALLEL_ARGFILE}-$v-api 2>&1" >> $PARALLEL_ARGFILE
+  echo "cat ${PARALLEL_ARGFILE}-$v-api" >> $PARALLEL_ARGFILE
+  echo "nvm exec $v yarn run cli-test > ${PARALLEL_ARGFILE}-$v-cli 2>&1" >> $PARALLEL_ARGFILE
+  echo "cat ${PARALLEL_ARGFILE}-$v-cli" >> $PARALLEL_ARGFILE
 done
 parallel -k --ungroup :::: $PARALLEL_ARGFILE
