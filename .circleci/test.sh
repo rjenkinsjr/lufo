75aab6b30cd4a9e6d45c88ce548de3d7c4b4e6f2
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-03-29 23:43:16 -0400

    Try another approach

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,7 +3,8 @@ set +e
 . ~/lufo/.lufoDependencies
 PARALLEL_ARGFILE="/tmp/lufoTest-$RANDOM"
 for v in "${LUFO_NODE_VERSIONS[@]}"; do
-  echo "nvm exec $v yarn run api-test > ${PARALLEL_ARGFILE}-$v-api 2>&1 && cat ${PARALLEL_ARGFILE}-$v-api" >> $PARALLEL_ARGFILE
-  echo "nvm exec $v yarn run cli-test > ${PARALLEL_ARGFILE}-$v-cli 2>&1 && cat ${PARALLEL_ARGFILE}-$v-cli" >> $PARALLEL_ARGFILE
+  echo "nvm exec $v yarn run api-test > ${PARALLEL_ARGFILE}-$v-api" >> $PARALLEL_ARGFILE
+  echo "nvm exec $v yarn run cli-test > ${PARALLEL_ARGFILE}-$v-cli" >> $PARALLEL_ARGFILE
 done
-parallel -k --ungroup :::: $PARALLEL_ARGFILE
+cat PARALLEL_ARGFILE="/tmp/lufoTest-$RANDOM"
+parallel --no-notice --keep-order --tag --group :::: $PARALLEL_ARGFILE
