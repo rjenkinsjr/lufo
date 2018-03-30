5f2e61cdf3a814b265a97611085207c6bf844f27
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-03-29 23:59:21 -0400

    Better formatting

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,6 +3,9 @@ set +e
 . ~/lufo/.lufoDependencies
 for v in "${LUFO_NODE_VERSIONS[@]}"; do
   echo "=== $v ==="
+  echo '--- API ---'
   nvm exec $v yarn run api-test
+  echo '--- CLI ---'
   nvm exec $v yarn run cli-test
+  echo
 done
