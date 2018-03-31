70cbb3a3889c88b134e643a08a7ae6b8d5c0b73e
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-03-30 20:28:48 -0400

    Work around yarn/node 7 warning

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+if [[ "$1" == "api" ]]; then
+  cd lufo-api
+  rm -rf lib
+  ../node_modules/.bin/babel -q src/ -d lib/
+  ../node_modules/.bin/flow-copy-source src lib
+elif [[ "$1" == "cli" ]]; then
+  cd lufo-cli
+  rm -rf lufo.js
+  ../node_modules/.bin/babel -q src/ -d .
+  chmod +x lufo.js
+else
+  echo 'ERROR: component not specified.'
+  exit 1
+fi
---
+++ scripts/lint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+if [[ "$1" == "api" ]]; then
+  cd lufo-api
+elif [[ "$1" == "cli" ]]; then
+  cd lufo-cli
+else
+  echo 'ERROR: component not specified.'
+  exit 1
+fi
+../node_modules/.bin/eslint src
