9e4f7a0982943020e87b0605936d26f8e1a26ff4
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-04-12 18:58:51 -0400

    Add more tests

--- lufo-api/spec/TcpClientSpec.js
+++ lufo-api/spec/TcpClientSpec.js
@@ -8,7 +8,7 @@ function sleep(ms) {
 }
 
 describe("TcpClient#constructorNonDefault", function() {
-  var server = net.createServer();
+  const server = net.createServer();
   const options = {
     localTcpPort: 1234,
     localHost: 'localhost',
@@ -34,17 +34,21 @@ describe("TcpClient#constructorNonDefault", function() {
   });
 });
 
-describe("TcpClient#constructor", function() {
-  var server = net.createServer();
+describe("TcpClient", function() {
+  var server, recv;
   beforeEach(async function() {
+    server = net.createServer();
+    recv = [];
+    server.on('connection', function(socket) {
+      socket.on('data', function(data) {
+        recv.push(data);
+      });
+    });
     server.listen(defaultPort, serverHost);
     while (!server.listening) await sleep(100);
   });
-  afterEach(async function() {
-    server.close();
-    while (server.listening) await sleep(100);
-  });
-  it("applies correct defaults", function() {
+  afterEach(function() { server.close(); });
+  it("#constructor applies correct defaults", function() {
     const client = new TcpClient(null, {host:serverHost});
     expect(client._options.localPort).toBe(-1);
     expect(client._options.localAddress).toBe('');
@@ -52,25 +56,67 @@ describe("TcpClient#constructor", function() {
     expect(client._options.remoteAddress).toBe(serverHost);
     expect(client._options.immediate).toBe(true);
   });
-});
-
-describe("TcpClient#connect", function() {
-  var server = net.createServer();
-  beforeEach(async function() {
-    server.listen(defaultPort, serverHost);
-    while (!server.listening) await sleep(100);
-  });
-  afterEach(async function() {
-    server.close();
-    while (server.listening) await sleep(100);
-  });
-  it("works", async function() {
-    const cb = { connect: function() {} };
-    spyOn(cb, 'connect');
+  it("#connect works", async function() {
+    const cb = jasmine.createSpy('connect');
     const client = new TcpClient(null, {host:serverHost});
-    client.connect(cb.connect);
+    client.connect(cb);
     await sleep(100);
-    expect(cb.connect).toHaveBeenCalled();
+    expect(cb).toHaveBeenCalled();
+  });
+  it("#on works", async function() {
+    const client = new TcpClient(null, {host:serverHost});
+    client.connect(function() { client.on(); });
+    await sleep(100);
+    expect(recv.length).toBe(1);
+    expect(recv[0]).toEqual(Buffer.from([0x71, 0x23, 0x0F, 0xA3]));
+  });
+  it("#off works", async function() {
+    const client = new TcpClient(null, {host:serverHost});
+    client.connect(function() { client.off(); });
+    await sleep(100);
+    expect(recv.length).toBe(1);
+    expect(recv[0]).toEqual(Buffer.from([0x71, 0x24, 0x0F, 0xA4]));
+  });
+  it("#rgbw works", async function() {
+    const client = new TcpClient(null, {host:serverHost});
+    client.connect(function() { client.rgbw(255, 255, 255, 255); });
+    await sleep(100);
+    expect(recv.length).toBe(1);
+    expect(recv[0]).toEqual(Buffer.from([0x31, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x0F, 0x3C]));
+  });
+  it("#rgbw clamps out-of-range values", async function() {
+    const client = new TcpClient(null, {host:serverHost});
+    client.connect(function() { client.rgbw(256, -1, 256, -1); });
+    await sleep(100);
+    expect(recv.length).toBe(1);
+    expect(recv[0]).toEqual(Buffer.from([0x31, 0xFF, 0x00, 0xFF, 0x00, 0x00, 0x0F, 0x3E]));
+  });
+  it("#builtin works", async function() {
+    const client = new TcpClient(null, {host:serverHost});
+    client.connect(function() { client.builtin('redGradualChange', 75); });
+    await sleep(100);
+    expect(recv.length).toBe(1);
+    expect(recv[0]).toEqual(Buffer.from([0x61, 0x26, 0x19, 0x0F, 0xAF]));
+  });
+  it("#builtin clamps out-of-range values", async function() {
+    const client = new TcpClient(null, {host:serverHost});
+    client.connect(function() { client.builtin('redGradualChange', 101); });
+    await sleep(100);
+    expect(recv.length).toBe(1);
+    expect(recv[0]).toEqual(Buffer.from([0x61, 0x26, 0x00, 0x0F, 0x96]));
+  });
+  it("#builtin rejects invalid function names", async function() {
+    const funcName = 'doesNotExist';
+    const cb = jasmine.createSpy('builtin');
+    const client = new TcpClient(null, {host:serverHost});
+    client.connect(function() { client.builtin(funcName, 0, cb); });
+    await sleep(100);
+    expect(recv.length).toBe(0);
+    expect(cb).toHaveBeenCalled();
+    expect(cb.calls.first().args.length).toBe(1);
+    const error = cb.calls.first().args[0];
+    expect(error instanceof Error).toBe(true);
+    expect(error.message).toBe(`No such built-in function ${funcName}`);
   });
 });
 
