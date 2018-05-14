266bb26d6d87bd188bdfa0a4968bc928fc133df9
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2018-05-13 21:39:34 -0400

    Found it!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,8 +28,8 @@ TMP_DIR="/tmp/lufo-cli-$RANDOM"
 mkdir $TMP_DIR
 tar -xzf $CLI_TARBALL -C $TMP_DIR
 echo 'Decompressing API package as a bundled dependency...'
-ROOT_DIR='package'
-TMP_API_DIR="$TMP_DIR/$ROOT_DIR/node_modules/lufo-api"
+PKG_ROOT_DIR='package'
+TMP_API_DIR="$TMP_DIR/$PKG_ROOT_DIR/node_modules/lufo-api"
 mkdir -p $TMP_API_DIR
 tar -xzf $API_TARBALL \
   --strip-components=1 \
@@ -37,12 +37,12 @@ tar -xzf $API_TARBALL \
   --exclude=README.md \
   --exclude=lib/*.flow
 echo 'Stripping API dependency and repackaging CLI package...'
-PACKAGE_JSON="$TMP_DIR/$ROOT_DIR/package.json"
+PACKAGE_JSON="$TMP_DIR/$PKG_ROOT_DIR/package.json"
 PACKAGE_JSON_TMP="$PACKAGE_JSON.tmp"
 cat $PACKAGE_JSON | jq 'del(.dependencies."lufo-api")' > $PACKAGE_JSON_TMP
 mv $PACKAGE_JSON_TMP $PACKAGE_JSON
 rm -f $CLI_TARBALL
-tar -czf $CLI_TARBALL -C $TMP_DIR $ROOT_DIR
+tar -czf $CLI_TARBALL -C $TMP_DIR $PKG_ROOT_DIR
 echo 'Done merging.'
 
 echo
