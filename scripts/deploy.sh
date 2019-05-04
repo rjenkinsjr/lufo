bd2465eaf81cac2212735bd2f33d62355c72ab93
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2019-05-04 00:47:21 -0400

    Commonize deploy version logic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$DIR/.."
 
+VERSION=$(cd $ROOT_DIR/lufo-api ; npx -c 'echo "$npm_package_version"')
+
 echo '--------------------------------------------------'
 echo '[nvm:deploy-version]'
 echo '--------------------------------------------------'
@@ -18,7 +20,6 @@ echo '--------------------------------------------------'
 cd $ROOT_DIR/lufo-api
 yarn --ignore-engines
 yarn run pack
-API_VERSION="$(cat package.json | jq -r '.version')"
 
 echo
 echo '--------------------------------------------------'
@@ -42,7 +43,7 @@ echo 'Pinning API dependency and repackaging CLI package...'
 PKG_ROOT_DIR='package'
 PACKAGE_JSON="$TMP_DIR/$PKG_ROOT_DIR/package.json"
 PACKAGE_JSON_TMP="$PACKAGE_JSON.tmp"
-cat $PACKAGE_JSON | jq ".dependencies.\"lufo-api\" = \"$API_VERSION\" | del(.scripts.postinstall)" > $PACKAGE_JSON_TMP
+cat $PACKAGE_JSON | jq ".dependencies.\"lufo-api\" = \"$VERSION\" | del(.scripts.postinstall)" > $PACKAGE_JSON_TMP
 mv $PACKAGE_JSON_TMP $PACKAGE_JSON
 rm -f $CLI_TARBALL
 tar -czf $CLI_TARBALL -C $TMP_DIR $PKG_ROOT_DIR
@@ -73,7 +74,6 @@ echo
 echo '--------------------------------------------------'
 echo '[tag]'
 echo '--------------------------------------------------'
-VERSION=$(cd $ROOT_DIR/lufo-api ; npx -c 'echo "$npm_package_version"')
 cd $ROOT_DIR
 git config user.email "do-not-reply@ronjenkins.info"
 git config user.name "CircleCI"
